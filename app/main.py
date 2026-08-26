400694c33cbdaaaa2d3d1d2c100c5bc4b8ce96ef
Bigya <bigyadhungana@gmail.com>
2026-08-26 14:46:27 +0545

    feat: migrate to version based endpoint

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
-import app.api.document as document
+from app.api.v1.router import api_router
+from app.api.health import router as health_router
 
 app = FastAPI(title="AI Reserach and Code Intelligence Platform")
 
-app.include_router(document.router)
+
+app.include_router(health_router)
+app.include_router(api_router)
 
 
 @app.get("/health")
